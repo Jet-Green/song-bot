058a7edc8e3fa0e9f9ef78c217729d33f380450e
Jet-Green <grishadzyin@gmail.com>
2026-03-13 12:38:36 +0500

    start server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import connectDB from './db/index.js';
 import { startBot } from './bot/index.js';
+import { app } from './webhook/index.js';
+import config from './config/index.js';
 
 const start = async () => {
   await connectDB();
   await startBot();
+  
+  console.log(`Webhook server running on port ${config.port}`);
 };
 
 start();
