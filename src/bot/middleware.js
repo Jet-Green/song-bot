2a04d8d7346bf76006190c8815a65a2fdc2a9720
Jet-Green <grishadzyin@gmail.com>
2026-03-13 10:50:23 +0500

    add logs

--- src/bot/middleware.js
+++ src/bot/middleware.js
@@ -1,11 +1,12 @@
 import config from '../config/index.js';
 
 export const isAdmin = (userId) => {
+  console.log('Checking admin:', userId, 'adminIds:', config.adminIds);
   return config.adminIds.includes(userId);
 };
 
 export const adminMiddleware = async (ctx, next) => {
-  if (isAdmin(ctx.from.id)) {
+  if (isAdmin(ctx.from?.id)) {
     return next();
   }
   return ctx.reply('У вас нет доступа к этой команде');
