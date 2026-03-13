77313fb4fe7bc0d5cc8da6d73a1646c8583c1f61
Jet-Green <grishadzyin@gmail.com>
2026-03-13 17:46:17 +0500

    add admin menu

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,7 +1,5 @@
 import { setupUserCommands } from './userCommands.js';
-import { setupAdminCommands } from './adminCommands.js';
 
 export const setupCommands = (bot, mainKeyboard) => {
-  setupAdminCommands(bot);
   setupUserCommands(bot, mainKeyboard);
 };
